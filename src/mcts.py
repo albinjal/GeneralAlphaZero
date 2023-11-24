74de0e20cc5a059b663cf84b955c92cc32bdcf28
Albin Jaldevik <albja664@student.liu.se>
2023-11-24 15:55:47 +0100

    document

--- src/mcts.py
+++ src/mcts.py
@@ -9,7 +9,12 @@ from policies import DefaultExpansionPolicy, Policy, SelectionPolicy
 ActionType = TypeVar("ActionType")
 ObservationType = TypeVar("ObservationType")
 
+
 class MCTS(Generic[ObservationType, ActionType]):
+    """
+    This class contains the basic MCTS algorithm without assumtions on the value function.
+    """
+
     env: gym.Env[ObservationType, ActionType]
     selection_policy: SelectionPolicy[ObservationType, ActionType]
     expansion_policy: Policy[
@@ -38,17 +43,21 @@ class MCTS(Generic[ObservationType, ActionType]):
 
     def build_tree(self, from_node: Node[ObservationType, ActionType], iterations: int):
         for _ in range(iterations):
+            # traverse the tree and select the node to expand
             selected_node_for_expansion, env = self.select_node_to_expand(from_node)
             # check if the node is terminal
             if selected_node_for_expansion.is_terminal():
-                eval_node = selected_node_for_expansion
+                # if the node is terminal, we can not expand it
+                # the value (sum of future reward) of the node is 0
+                # the backprop will still propagate the visit and reward
+                selected_node_for_expansion.backprop(0)
             else:
                 # expand the node
                 eval_node = self.expand(selected_node_for_expansion, env)
-            # evaluate the node
-            value = self.value_function(eval_node, env)
-            # backpropagate the value
-            eval_node.backprop(value)
+                # evaluate the node
+                value = self.value_function(eval_node, env)
+                # backpropagate the value
+                eval_node.backprop(value)
         return from_node
 
     def value_function(
@@ -56,7 +65,10 @@ class MCTS(Generic[ObservationType, ActionType]):
         node: Node[ObservationType, ActionType],
         env: gym.Env[ObservationType, ActionType],
     ) -> float:
-        return node.reward
+        """The point of the value function is to estimate the value of the node.
+        The value is defined as the expected future reward if we get to the node given some policy.
+        """
+        return 0.0
 
     def select_node_to_expand(
         self, from_node: Node[ObservationType, ActionType]
@@ -68,14 +80,20 @@ class MCTS(Generic[ObservationType, ActionType]):
         """
 
         node = from_node
-
+        # the reason we copy the env is because we want to keep the original env in the root state
+        # Question: note that all envs will have the same seed, this might needs to be dealt with for stochastic envs
         env = copy.deepcopy(self.env)
         while not node.is_terminal():
+            # select which node to step into
             action = self.selection_policy(node)
+            # if the selection policy returns None, this indicates that the current node should be expanded
             if action is None:
                 return node, env
+            # step into the node
             node = node.step(action)
             # also step the environment
+            # Question: right now we do not save the observation or reward from the env since we already have them saved
+            # This might be worth considering though if we use stochastic envs since the rewards/states could vary each time we execute an action sequence
             env.step(action)
 
         return node, env
@@ -88,6 +106,7 @@ class MCTS(Generic[ObservationType, ActionType]):
         """
         Expands the node and returns the expanded node.
         """
+        # in the default case we sample a random unexplored action
         action = self.expansion_policy(node)
         # step the environment
         observation, reward, terminated, truncated, _ = env.step(action)
@@ -104,8 +123,6 @@ class MCTS(Generic[ObservationType, ActionType]):
         return new_child
 
 
-
-
 class RandomRolloutMCTS(MCTS):
     def __init__(self, rollout_budget=40, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -116,9 +133,12 @@ class RandomRolloutMCTS(MCTS):
         node: Node[ObservationType, ActionType],
         env: gym.Env[ObservationType, ActionType],
     ) -> float:
-        # if the node is terminal, return the reward
+        """
+        The standard value function for MCTS is the the sum of the future reward when acting with uniformly random policy.
+        """
+        # if the node is terminal, return 0
         if node.is_terminal():
-            return node.reward
+            return 0.0
 
         # if the node is not terminal, simulate the enviroment with random actions and return the accumulated reward until termination
         accumulated_reward = 0.0
